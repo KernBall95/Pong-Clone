51b84f5790b2210b8dfa2ff8e942e8e24496a02b
Kern Ball <kern.ball@yahoo.co.uk>
2018-11-01 01:41:40 +0000

    Made a start
    Just made a start. Paddles are moving and so is the ball. Next up is fixing the ball collision and bouncing.

---
+++ Assets/Ball.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour {
+
+    Rigidbody2D rb;
+    public float ballSpeed;
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody2D>();
+        rb.velocity = new Vector2(ballSpeed, ballSpeed / 5);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+       // rb.velocity /= 2;
+        Debug.Log("Ball Collided!");
+    }
+}
---
+++ Assets/PaddleMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PaddleMovement : MonoBehaviour {
+
+    float yPos;
+    public float paddleSpeed;
+    bool paddleCollidingTop, paddleCollidingBottom;
+
+	// Use this for initialization
+	void Start () {
+        yPos = transform.position.y;
+        paddleCollidingTop = false;
+        paddleCollidingBottom = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if(this.name == "PaddleOne")
+        {
+            if (Input.GetKey(KeyCode.W) && !paddleCollidingTop)
+            {
+                yPos += paddleSpeed;
+                transform.position = new Vector2(transform.position.x, yPos);
+            }
+
+            if (Input.GetKey(KeyCode.S) && !paddleCollidingBottom)
+            {
+                yPos -= paddleSpeed;
+                transform.position = new Vector2(transform.position.x, yPos);
+            }
+        }
+        
+        if(this.name == "PaddleTwo")
+        {
+            if (Input.GetKey(KeyCode.UpArrow) && !paddleCollidingTop)
+            {
+                yPos += paddleSpeed;
+                transform.position = new Vector2(transform.position.x, yPos);
+            }
+
+            if (Input.GetKey(KeyCode.DownArrow) && !paddleCollidingBottom)
+            {
+                yPos -= paddleSpeed;
+                transform.position = new Vector2(transform.position.x, yPos);
+            }
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if(other.gameObject.tag == "WallTop")
+        {
+            paddleCollidingTop = true;
+        }
+        else if(other.gameObject.tag == "WallBottom")
+        {
+            paddleCollidingBottom = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        paddleCollidingTop = false;
+        paddleCollidingBottom = false;
+    }
+}
